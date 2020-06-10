7c08005aece64d2d8b86e46cfd52e9c1e66905de
Alisha Das <dasalisha9@gmail.com>
2020-06-11 00:51:11 +0530

    Updates

---
+++ precache-manifest.8f6c12e937d4703ee124a25d3258b9ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef937eca4084938126eb2dc0c6e8a23",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "f84a27a27f19a859f5b9",
+    "url": "/robofriends/static/css/main.d941b768.chunk.css"
+  },
+  {
+    "revision": "7f44a659fbae42a2eaf8",
+    "url": "/robofriends/static/js/2.f62f56c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f62f56c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f84a27a27f19a859f5b9",
+    "url": "/robofriends/static/js/main.0f3ad4ef.chunk.js"
+  },
+  {
+    "revision": "73cdd2d039794993d904",
+    "url": "/robofriends/static/js/runtime-main.29a9c35e.js"
+  },
+  {
+    "revision": "751a0e4b9f683e9539a39ff766070073",
+    "url": "/robofriends/static/media/RockFont.751a0e4b.woff"
+  }
+]);
